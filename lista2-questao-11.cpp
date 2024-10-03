12de604f18a59c0a32cee607fa71e09de0896366
rafaellourenco11 <rafaeloliveiradelourenco@gmail.com>
2024-10-03 11:35:52 -0300

    Add files via upload

---
+++ lista2-questao-11.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <locale.h>
+
+/*
+    ** Função : Recebe três números inteiros e imprime os números em ordem crescente.
+    ** Autor : Rafael de Oliveira Lourenço
+    ** Data : 03/10/2024
+    ** Observações: O programa compara os três números e exibe o maior.
+*/
+
+int main() {
+	setlocale(LC_ALL, "");
+	
+    int num1, num2, num3, temp;
+
+    printf("Digite o primeiro número: ");
+    scanf("%d", &num1);
+
+    printf("Digite o segundo número: ");
+    scanf("%d", &num2);
+
+    printf("Digite o terceiro número: ");
+    scanf("%d", &num3);
+
+    if (num1 > num2) {
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    
+    if (num1 > num3) {
+        temp = num1;
+        num1 = num3;
+        num3 = temp;
+    }
+
+    if (num2 > num3) {
+        temp = num2;
+        num2 = num3;
+        num3 = temp;
+    }
+
+    printf("Os números em ordem crescente são: %d, %d, %d\n", num1, num2, num3);
+
+    return 0;
+}
+
+
+
+
+	return 0;	
+}
